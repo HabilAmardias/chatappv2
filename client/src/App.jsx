94a0e6586d8609d87eab2b64d8bda9b784f88b47
Muhammad Habil Amardias <habilamar@gmail.com>
2023-01-24 17:36:40 +0700

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Head from './components/Head';
 import './App.css'
+import Cookies from 'js-cookie';
 
 function App() {
-
-  return (
-    <div className="App">
-
-    </div>
-  )
+  const { userId } = useParams();
+  const jwt = Cookies.get('jwt')
+  if (jwt) {
+    return (
+      <>
+        <Head />
+        <Navbar userId={userId} />
+        <main />
+      </>
+    )
+  }
 }
 
 export default App
